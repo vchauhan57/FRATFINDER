6f7782bc3b7772237c9446353807b389f4a53039
Vishal Vijay <vishvj04@berkeley.edu>
2024-08-03 19:20:38 -0700

    test1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome FUCKHEAD</ThemedText>
+        <ThemedText type="title">Welcome BROTHER</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
