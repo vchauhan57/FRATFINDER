9670ded0725106d59ad77ea7de7899fb1601a186
Vishal Vijay <vishvj04@berkeley.edu>
2024-08-04 23:01:15 -0700

    working gradient overlay

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, Image, Dimensions } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
-import {LinearGradient} from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const IndexScreen = () => {
     const initialCards = [
         { id: 1, name: "Kendrick Lamar", bio: "Engineering Major at XYZ University. Enjoys hiking and outdoor activities.", image: require('../../assets/images/kendrick.jpg') },
         { id: 2, name: "Stephen Curry", bio: "Biology Major at XYZ University. Loves painting and photography.", image: require('../../assets/images/steph.jpeg') },
-        { id: 3, name: "Lebron James", bio: "You are my sunshine!", image: require('../../assets/images/lebron.jpeg')},
+        { id: 3, name: "LeBron James", bio: "You are my sunshine!", image: require('../../assets/images/lebron.jpeg')},
+        { id: 4, name: "Abel Tesfaye", bio: "We had s*x in the studio, nobody's watching", image: require('../../assets/images/abeltesfaye.jpeg')},
     ];
 
     const [cards, setCards] = useState(initialCards);
@@ -25,14 +26,14 @@ const IndexScreen = () => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.logo}>FratFinder</Text>
+            <Text style={styles.logo}>fratch.</Text>
             <Swiper
                 cards={cards}
                 renderCard={(card) => (
                     <View style={styles.card}>
                         <Image style={styles.image} source={card.image} />
                         <LinearGradient
-                            colors={['transparent', 'rgba(0,0,0,0.8)']} // Adjust gradient colors as needed
+                            colors={['transparent', 'rgba(0,0,0,0.7)', 'rgba(0,0,0,0.9)', 'rgba(0,0,0,1.1)']}
                             style={styles.gradientOverlay}
                         >
                             <Text style={styles.name}>{card.name}</Text>
@@ -97,7 +98,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#050505', // This is the container background color
+        backgroundColor: '#050505',
         paddingTop: 30
     },
     logo: {
@@ -106,31 +107,31 @@ const styles = StyleSheet.create({
         left: 20,
         fontSize: 30,
         fontWeight: 'bold',
-        color: 'gold'
+        color: '#91760d'
     },
     card: {
         width: Dimensions.get('window').width - 50,
         height: Dimensions.get('window').height - 200,
         borderRadius: 8,
-        backgroundColor: '#FFFFFF', // Might not need this
-        justifyContent: 'flex-end', // Align items to the bottom
+        backgroundColor: 'transparent',
+        justifyContent: 'flex-end',
         alignItems: 'center',
         padding: 10,
         marginTop: -10,
         margin: 5,
-        overflow: 'hidden', // Important for borderRadius
+        overflow: 'hidden',
     },
     image: {
         width: '100%',
-        height: '100%', // Change to full height of the card
-        position: 'absolute', // Position it absolutely to cover the whole card
-        borderRadius: 30,
+        height: '100%',
+        position: 'absolute',
+        borderRadius: 40,
     },
     gradientOverlay: {
         width: '100%',
-        height: '50%', // Cover the lower 50% of the image
+        height: '30%', // Increase this percentage to cover more of the image, e.g., 60% or 70%
         position: 'absolute',
-        bottom: 0, // Start from the bottom of the card
+        bottom: 0,
         justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
@@ -138,11 +139,11 @@ const styles = StyleSheet.create({
     name: {
         fontSize: 24,
         fontWeight: 'bold',
-        color: '#fff', // Ensuring text is visible on the darker gradient
+        color: '#fff',
     },
     bio: {
         fontSize: 18,
-        color: '#fff', // Ensuring text is visible on the darker gradient
+        color: '#fff',
         textAlign: 'center',
     }
 });
